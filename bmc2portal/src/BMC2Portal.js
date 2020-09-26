ddd03769ad04a947c45dd2fca5dc80e0a50b393e
jmccarthy <jmccarthy@shoc.us>
2020-09-26 08:57:39 -0500

    enabled prettier

--- bmc2portal/src/BMC2Portal.js
+++ bmc2portal/src/BMC2Portal.js
@@ -1,31 +1,31 @@
-import React from 'react';
-  
-import { Router, Route } from 'react-router'
-import { createBrowserHistory } from 'history'
+import React from "react";
+
+import { Router, Route } from "react-router";
+import { createBrowserHistory } from "history";
 
 // import logo from './logo.svg';
-import './css/body.css';
-import './css/styles.css';
-import './css/fonts.css';
-import './css/sidebar.css';
-import './css/spoiler.css';
-import './css/chips.css';
+import "./css/body.css";
+import "./css/styles.css";
+import "./css/fonts.css";
+import "./css/sidebar.css";
+import "./css/spoiler.css";
+import "./css/chips.css";
 
-import SideBar from './components/sidebar.js'
-import AirspaceList from './components/missioncrew/airspacelist.js'
-import Home from './components/home.js'
+import SideBar from "./components/sidebar.js";
+import AirspaceList from "./components/missioncrew/airspacelist.js";
+import Home from "./components/home.js";
 
 let browserHistory = createBrowserHistory();
 
 function BMC2Portal() {
   return (
     <div className="app">
-      <SideBar/>
+      <SideBar />
       <div className="body-content">
-      <Router history={browserHistory}>
-        <Route path="/" component={Home} />
-        <Route path="/msncrew/airspaces.html" component={AirspaceList} />
-      </Router>
+        <Router history={browserHistory}>
+          <Route exact path="/" component={Home} />
+          <Route path="/msncrew/airspaces.html" component={AirspaceList} />
+        </Router>
       </div>
       <header>
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
