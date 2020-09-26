0c02212415c6436d429a229eead7dc66135ae8b6
jmccarthy <jmccarthy@shoc.us>
2020-09-26 14:48:42 -0500

    initial resource list implementation

---
+++ bmc2portal/src/components/resourcelist.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import '../css/sidebar.css';
+
+/**
+ * This Component contains the rendering of the "Links & Resourcs" 'page'.
+ */
+class ResourceList extends React.Component {
+  render(){
+    this.elems = [];
+    return (
+      <div>
+          This is where links go.
+      </div>
+    )}
+}
+
+export default ResourceList
