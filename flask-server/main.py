70f3e38a15e6724509334d3af4970480a3d5ac60
jmccarthy <jmccarthy@shoc.us>
2020-09-26 14:28:45 -0500

    flask always render index

--- flask-server/main.py
+++ flask-server/main.py
@@ -2,8 +2,8 @@ from flask import (Flask, render_template)
 
 app = Flask("__main__")
 
-@app.route("/")
-def my_index():
+@app.route("/<path:path>")
+def my_index(path):
     return render_template("index.html")
 
 app.run(debug=True)
