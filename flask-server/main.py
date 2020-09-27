e6537370d49dadc66a50bf89e863835be975d8ad
jmccarthy <jmccarthy@shoc.us>
2020-09-27 12:50:37 -0500

    remove flask-server

--- flask-server/main.py
+++
@@ -1,9 +0,0 @@
-from flask import (Flask, render_template)
-
-app = Flask("__main__")
-
-@app.route("/<path:path>")
-def my_index(path):
-    return render_template("index.html")
-
-app.run(debug=True)
