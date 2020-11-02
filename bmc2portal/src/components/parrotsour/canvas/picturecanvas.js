f6da1666231fd8625502c84a8053ccd9f5313c18
jmccarthy <jmccarthy@shoc.us>
2020-11-01 23:45:27 -0600

    orientation and braa toggle

--- bmc2portal/src/components/parrotsour/canvas/picturecanvas.js
+++ bmc2portal/src/components/parrotsour/canvas/picturecanvas.js
@@ -4,11 +4,18 @@ import Canvas from './canvas.js'
 
 export default class PictureCanvas extends React.Component {
 
-    draw(){
-
+    draw(context, frameCount, canvas){
+        console.log("draw")
+        context.beginPath()
+        context.arc(100, 75, 50, 0, 2 * Math.PI);
+        context.stroke()
     }
 
     render(){
-        return <Canvas draw={this.draw} />
+        return <Canvas 
+            draw={this.draw} 
+            height={this.props.height} 
+            width={this.props.width} 
+            braaFirst={this.props.braaFirst} />
     }
 }
